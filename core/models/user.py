d2b403e07faa5f4075b5f2d40d0db9f54b6128c1
Oleksii-Op <oleksii.oprish@gmail.com>
2025-02-04 16:54:09 +0200

    Validation for user field

--- core/models/user.py
+++ core/models/user.py
@@ -1,4 +1,19 @@
+from typing import Annotated
+
 from sqlmodel import Field, SQLModel
+from pydantic import AfterValidator
+import re
+
+
+def validate_username_or_name(value: str) -> str:
+    if not re.match(r"^[a-zA-Z0-9]+$", value):
+        raise ValueError(
+            "Can only contain letters and numbers",
+        )
+    return value
+
+
+ValidString = Annotated[str, AfterValidator(validate_username_or_name)]
 
 
 class User(SQLModel, table=True):
@@ -7,8 +22,17 @@ class User(SQLModel, table=True):
         primary_key=True,
         index=True,
     )
-    name: str
-    username: str
-    age: int | None = None
+    name: ValidString = Field(
+        min_length=0,
+        max_length=20,
+    )
+    username: ValidString = Field(
+        min_length=8,
+        max_length=20,
+    )
+    age: int | None = Field(
+        default=None,
+        gt=0,
+    )
 
     __table_args__ = {"extend_existing": True}
