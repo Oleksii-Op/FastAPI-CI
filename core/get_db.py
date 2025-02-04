309cd0cb573dabd8168fd77506f479587c4d5c25
Oleksii-Op <oleksii.oprish@gmail.com>
2025-02-04 15:28:20 +0200

    added db config + session getter

---
+++ core/get_db.py
@@ -0,0 +1,58 @@
+from sqlmodel import create_engine, SQLModel
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy import Engine
+from typing import Generator
+from core.config import settings
+from core.models.user import User  # type: ignore
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class DatabaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        pool_size: int = 5,
+        max_overflow: int = 10,
+    ) -> None:
+        self.engine: Engine = create_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
+        )
+        self.session_factory: sessionmaker[Session] = sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    def dispose(self) -> None:
+        self.engine.dispose()
+
+    def create_database(self) -> None:
+        SQLModel.metadata.create_all(self.engine)
+        logger.warning("Database creation has finished")
+
+    def dispose_database(self) -> None:
+        SQLModel.metadata.drop_all(self.engine)
+        logger.warning("Database deletion has been successful")
+
+    def session_getter(self) -> Generator[Session, None, None]:
+        with self.session_factory() as session:
+            logger.info("Yielding session")
+            yield session
+
+
+get_db = DatabaseHelper(
+    settings.db.url,
+    echo=settings.db.echo,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow,
+    echo_pool=settings.db.echo_pool,
+)
